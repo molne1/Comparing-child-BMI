4637716535891d186a8cd4707c48fe95efca7663
molne1 <138243542+molne1@users.noreply.github.com>
2024-03-28 11:10:24 +0100

    Update app.py
    - Added the option of displaying one child's bmi on the graph
    - Added the IOTF which was missing before 
    - Changed formatting and colors.

--- app.py
+++ app.py
@@ -38,26 +38,35 @@ app.layout = html.Div([
     html.Div([
         dcc.Graph(id='graph-container')
     ]),
-    html.Div([dcc.RadioItems(id = 'radio_item_sex', options = ['girl', 'boy'], value = 'girl', inline=True)],
-             ),
+  html.Div([html.H6("Choose sex"),
+               dcc.RadioItems(id = 'radio_item_sex', options = ['girl', 'boy'], value = 'girl', inline=True, 
+                            )], style={'border': '1px solid grey', 'padding': '4px', 'border-radius': '5px',
+                                       'display': 'inline-block'} ),
+    html.Div([
+        html.H6("If you want to display a child, choose age and BMI"),
+        dcc.Input(id = 'age', placeholder='Enter age in years', type='number', value='',min=0, max=20,),
+        dcc.Input(id = 'BMI', placeholder='Enter BMI', type='number', value='', min=10, max=70)],
+        style = {'border': '1px solid grey', 'padding': '4px', 'border-radius': '5px', 'display': 'inline-block'}),
+    
+    
      html.Div([
         html.Div(
             style={'width': '20%', 'height': '100%', 'float': 'left'},
             children=[
-                html.H6("World Health Organisation"),
+                html.H4("World Health Organisation (WHO)", style={'width': '40%','color': '#009CDE'}),
                 dcc.Checklist(
                     id='checkbox-container_WHO',
                     options=[
                         {'label': 'SD0', 'value': 'SD0'},
-                        {'label': 'SD1', 'value': 'SD1'},
-                        {'label': 'SD2', 'value': 'SD2'},
+                        {'label': 'SD1 - overweight*', 'value': 'SD1'},
+                        {'label': 'SD2 - obesity*', 'value': 'SD2'},
                         {'label': 'SD3', 'value': 'SD3'},
                         {'label': 'SD4', 'value': 'SD4'},
                         {'label': 'SD5', 'value': 'SD5'},
                         {'label': 'SD6', 'value': 'SD6'},
                         {'label': 'SD7', 'value': 'SD7'},
                         {'label': 'SD8', 'value': 'SD8'},
-                    ],value =['SD0'],
+                    ],value =['SD1'],
                   
                 ),
             ]
@@ -65,19 +74,19 @@ app.layout = html.Div([
         html.Div(
             style={'width': '20%', 'height': '100%', 'float': 'left', 'display': 'inline-block'},
             children=[
-                html.H6("International Obesity Taskforce"),
+                html.H4("International Obesity Taskforce", style={'width': '40%','color': 'green'}),
                 dcc.Checklist(
                     id='checkbox-container_IOTF',
                     options=[
-                        {'label': 'BMI25', 'value': 'BMI_25'},
-                        {'label': 'BMI30', 'value': 'BMI_30'},
-                        {'label': 'BMI35', 'value': 'BMI_35'},
+                        {'label': 'BMI25 - overweight', 'value': 'BMI_25'},
+                        {'label': 'BMI30 - obesity', 'value': 'BMI_30'},
+                        {'label': 'BMI35 - severe obesity', 'value': 'BMI_35'},
                         {'label': 'BMI40', 'value': 'BMI_40'},
                         {'label': 'BMI45', 'value': 'BMI_45'},
                         {'label': 'BMI50', 'value': 'BMI_50'},
                         {'label': 'BMI55', 'value': 'BMI_55'},
                         {'label': 'BMI60', 'value': 'BMI_60'},
-                    ],
+                    ],value = ['BMI_25']
                  
                 )
             ]
@@ -85,7 +94,7 @@ app.layout = html.Div([
         html.Div(
             style={'width': '20%', 'height': '100%', 'float': 'left', 'display': 'inline-block'},
             children=[
-                html.H6("CDC zscore (extended)"),
+                html.H4("CDC zscore (Extended)", style={'width': '40%', 'color': 'red'}),
                 dcc.Checklist(
                     id='checkbox-container_CDC',
                     options=[
@@ -96,7 +105,7 @@ app.layout = html.Div([
                         {'label': 'SD4', 'value': 'SD4'},
                         {'label': 'SD5', 'value': 'SD5'},
          
-                    ], value =['SD0'],
+                    ], value =['SD1'],
                   
                 )
             ]
@@ -104,14 +113,14 @@ app.layout = html.Div([
         html.Div(
             style={'width': '20%', 'height': '100%', 'float': 'left', 'display': 'inline-block'},
             children=[
-                html.H6("CDC % above 95 percentile"),
+                 html.H4("CDC % above 95 percentile", style={'width': '40%','color': 'maroon'}),
                 dcc.Checklist(
                     id='checkbox-container_CDC95P',
                     options=[
-                        {'label': '85P', 'value': 'P85'},
-                        {'label': '95P', 'value': 'P95'},
-                        {'label': '120%', 'value': 'pct120ofP95'},
-                        {'label': '140%', 'value': 'pct140ofP95'},
+                        {'label': '85 percentile - overweight', 'value': 'P85'},
+                        {'label': '95 percentile - obesity class I', 'value': 'P95'},
+                        {'label': '120% - obesity class II', 'value': 'pct120ofP95'},
+                        {'label': '140% - obesity class III', 'value': 'pct140ofP95'},
                         {'label': '150%', 'value': 'pct150ofP95'},
                         {'label': '160%', 'value': 'pct160ofP95'},
                         {'label': '180%', 'value': 'pct180ofP95'},
@@ -127,6 +136,8 @@ app.layout = html.Div([
     Output(component_id='graph-container', component_property='figure'),
     
     [Input(component_id = 'radio_item_sex', component_property = 'value'),
+     Input(component_id = 'age', component_property = 'value'),
+    Input(component_id = 'BMI', component_property = 'value'),
      
     Input(component_id = 'checkbox-container_WHO', component_property='value'), 
     Input(component_id = 'checkbox-container_IOTF', component_property = 'value'),
@@ -134,23 +145,24 @@ app.layout = html.Div([
     Input(component_id = 'checkbox-container_CDC95P', component_property = 'value'),
     ]
 )
-def update_graph(sex, WHO, IOTF, CDC, CDC95P):
-    print(sex, WHO, IOTF, CDC, CDC95P)
+def update_graph(sex, age, bmi, WHO, IOTF, CDC, CDC95P):
+
     if sex  == 'girl':
         WHO_axes = girls['WHO']
-#         IOTF = girls['IOTF']
+        IOTF = girls['IOTF']
         CDC_axes = girls['CDC']
         CDC95P_axes = girls['CDC_pct']  
 
     else:
         WHO_axes = boys['WHO']
-#         IOTF = boys['IOTF']
+        IOTF = boys['IOTF']
         CDC_axes = boys['CDC']
         CDC_95P = boys['CDC_pct']
-    colors = ['blue', 'red']
+
 
     fig = go.Figure()
 
+#WHO
     for level in reversed(WHO):
         fig.add_trace(go.Scatter(x=WHO_axes['age_months'], y=WHO_axes[level], mode='lines', name=level,
                                  line=dict(color='#009CDE'), showlegend=False))
@@ -160,10 +172,17 @@ def update_graph(sex, WHO, IOTF, CDC, CDC95P):
         max_value_x = WHO_axes[(WHO_axes['age_months'] == placement_y)][level].iloc[0]
         fig.add_annotation(x=placement_y, y=max_value_x, text=label, showarrow=False, font=dict(size=8), bgcolor="white",
                            bordercolor = '#009CDE', borderwidth=1)
-#     for level in reversed(IOTF):
-#         print(level)
+#IOTF       
+    for level in reversed(IOTF):
+        fig.add_trace(go.Scatter(x=IOTF_axes['age_months'], y=IOTF_axes[level], mode='lines', name=level,
+                                     line=dict(color='green'), showlegend=False))
 
-
+        label = f'IOTF-{level}'
+        placement_y = 192
+        max_value_x = IOTF_axes[(IOTF_axes['age_months'] == placement_y)][level].iloc[0]
+        fig.add_annotation(x=placement_y, y=max_value_x, text=label, showarrow=False, font=dict(size=8), bgcolor="white",
+                       bordercolor = 'green', borderwidth=1)
+#CDC
     for level in reversed(CDC):
         fig.add_trace(go.Scatter(x=CDC_axes['age_months'], y=CDC_axes[level], mode='lines', name=level,
                                  line=dict(color='red'),showlegend=False))
@@ -173,7 +192,7 @@ def update_graph(sex, WHO, IOTF, CDC, CDC95P):
         max_value_x = CDC_axes[(CDC_axes['age_months'] == placement_y)][level].iloc[0]
         fig.add_annotation(x=placement_y, y=max_value_x, text=label, showarrow=False, font=dict(size=8), bgcolor="white",
                             bordercolor="red", borderwidth=1)
-        
+#CDCP95      
     for level in reversed(CDC95P):
         fig.add_trace(go.Scatter(x=CDC95P_axes['age_months'], y=CDC95P_axes[level], mode='lines', name=level,
                                  line=dict(color='maroon'),showlegend=False))
@@ -202,6 +221,17 @@ def update_graph(sex, WHO, IOTF, CDC, CDC95P):
         yaxis=dict(title=ylabel, titlefont=dict(size=fs_label),gridcolor='lightgrey'),
         xaxis_title=xlabel,
         yaxis_title=ylabel)
+    
+# plot child
+    if age is not None and bmi is not None:
+            fig.add_trace(go.Scatter(
+                x=[age*12],
+                y=[bmi],
+                mode='markers',
+                marker=dict(color='black', size=6)
+                ,showlegend=False,
+                name='Child'
+            ))
     
     return fig
 
